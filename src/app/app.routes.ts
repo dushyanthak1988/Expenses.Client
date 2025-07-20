600b777c7dd9350e04766e2f39d83800c1095bbf
Dushyantha Kalehewatte <dushyanthak@outlook.com>
2025-07-20 17:09:40 +0530

    add components Header , footer, login , Signup , transaction form , transaction list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
+import { TransactionFormComponent } from './components/transaction-form/transaction-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'transactions', component: TransactionListComponent },
+    { path: 'add', component: TransactionFormComponent },
+    { path: 'edit/:id', component: TransactionFormComponent }, // with dynamic route parameter
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '**', redirectTo: 'login' } // wildcard route for invalid paths
+];
